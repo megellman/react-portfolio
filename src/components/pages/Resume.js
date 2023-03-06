c6a8b4acc9b0f70a37a5cf33a44b631b477c9ff0
Megan Ellman <meganlellman@gmail.com>
2023-03-06 09:17:36 -0800

    added resume

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -3,7 +3,13 @@ import React from 'react';
 function Resume() {
     return (
         <div>
-
+            <a href='../../assets/resume' download='MeganEllman'>
+                Download Resume
+            </a>
+            <h3>Proficencies</h3>
+            <ul>
+                <li></li>
+            </ul>
         </div>
     )
 }
