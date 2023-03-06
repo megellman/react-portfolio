414576c7351d97a78965ddaf4eb263d2a5067928
Megan Ellman <meganlellman@gmail.com>
2023-03-05 19:16:04 -0800

    added footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-target-blank */
 import React from 'react';
 
 function Footer() {
@@ -5,13 +6,13 @@ function Footer() {
         <div>
             <ul className="nav justify-content-center">
                 <li className="nav-item">
-                    <a className="nav-link" href="https://www.linkedin.com/in/megan-ellman/">LinkedIn</a>
+                    <a className="nav-link  text-secondary" target='_blank' href="https://www.linkedin.com/in/megan-ellman/" >LinkedIn</a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link" href="https://github.com/megellman">GitHub</a>
+                    <a className="nav-link  text-secondary" target='_blank' href="https://github.com/megellman">GitHub</a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link" href="https://stackexchange.com/users/27213381/megan-ellman">Stack Overflow</a>
+                    <a className="nav-link  text-secondary" target='_blank' href="https://stackexchange.com/users/27213381/megan-ellman">Stack Overflow</a>
                 </li>
             </ul>
         </div>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,16 +4,16 @@ function Navigation({ page, pageChange }) {
     return (
         <ul className="nav justify-content-end">
             <li className="nav-item">
-                <a className={page === 'AboutMe' ? 'nav-link': 'nav-link disabled'} href="#aboutMe" onClick={() => pageChange('AboutMe')}>About Me</a>
+                <a className={page === 'AboutMe' ? 'nav-link disabled text-primary': 'nav-link text-secondary'} href="#aboutMe" onClick={() => pageChange('AboutMe')}>About Me</a>
             </li>
             <li className="nav-item">
-                <a className={page === 'Contact' ? 'nav-link' : 'nav-link disabled'} href="#contact" onClick={() => pageChange('Contact')}>Contact</a>
+                <a className={page === 'Contact' ? 'nav-link disabled text-primary' : 'nav-link text-secondary'} href="#contact" onClick={() => pageChange('Contact')}>Contact</a>
             </li>
             <li className="nav-item">
-                <a className={page === 'Portfolio' ? 'nav-link' : 'nav-link disabled'} href="#portfolio" onClick={() => pageChange('Portfolio')}>Portfolio</a>
+                <a className={page === 'Portfolio' ? 'nav-link disabled text-primary' : 'nav-link text-secondary'} href="#portfolio" onClick={() => pageChange('Portfolio')}>Portfolio</a>
             </li>
             <li className="nav-item">
-                <a className={page === 'Resume' ? 'nav-link' : 'nav-link disabled'} href='#resume' onClick={() => pageChange('Resume')}>Resume</a>
+                <a className={page === 'Resume' ? 'nav-link disabled text-primary' : 'nav-link text-secondary'} href='#resume' onClick={() => pageChange('Resume')}>Resume</a>
             </li>
         </ul>
     )
