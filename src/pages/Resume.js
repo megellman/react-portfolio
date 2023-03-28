5e7f94ea3a6058abe0d31d246589a50ae0b58176
Megan Ellman <meganlellman@gmail.com>
2023-03-28 12:44:12 -0700

    resume download link bolds on hover:

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -8,28 +8,30 @@ function Resume() {
         <div>
             <h2>Resume</h2>
             <a href={require('../assets/resume.pdf')} download='MeganEllman'
-             style={{color: "darkorchid", textDecoration: "none"}}>
+                style={{ color: "darkorchid", textDecoration: "none" }}
+                onMouseOver={(e) => e.target.style.fontWeight = "bold"}
+                onMouseOut={(e) => e.target.style.fontWeight = "normal"}>
                 Download Resume
             </a>
             <h3>Proficencies</h3>
             <div className='row row-cols-1 row-cols-md-2 g-4'>
-            <div className='col-6'>
-             <h4>Front-end</h4>
-            <ul>
-                {frontEnd.map((item, i) => (
-                    <li key={i}>{item}</li>
-                ))}
-            </ul>
-            </div>
-            <div className='col-6'>
-            <h4>Back-end</h4>
-            <ul>
-                {backEnd.map((item, i) => (
-                    <li key={i}>{item}</li>
-                ))}
-            </ul> 
-              
-            </div>
+                <div className='col-6'>
+                    <h4>Front-end</h4>
+                    <ul>
+                        {frontEnd.map((item, i) => (
+                            <li key={i}>{item}</li>
+                        ))}
+                    </ul>
+                </div>
+                <div className='col-6'>
+                    <h4>Back-end</h4>
+                    <ul>
+                        {backEnd.map((item, i) => (
+                            <li key={i}>{item}</li>
+                        ))}
+                    </ul>
+
+                </div>
             </div>
         </div>
     )
