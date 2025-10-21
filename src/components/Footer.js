115a7e8ce1037344093e316e24f3698e0ba076c9
Megan Ellman <meganlellman@gmail.com>
2025-10-21 09:28:58 -0700

    removed stackoverflow link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,14 +5,11 @@ function Footer() {
     return (
         <div className='footer navbar justify-content-center'>
             <ul className="nav ">
-                <li className="nav-item">
+                <li className="nav-item hover-link">
                     <a className="nav-link  text-secondary" target='_blank' href="https://www.linkedin.com/in/megan-ellman/" >LinkedIn</a>
                 </li>
-                <li className="nav-item">
+                <li className="nav-item hover-link">
                     <a className="nav-link  text-secondary" target='_blank' href="https://github.com/megellman">GitHub</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link  text-secondary" target='_blank' href="https://stackexchange.com/users/27213381/megan-ellman">Stack Overflow</a>
                 </li>
             </ul>
         </div>
