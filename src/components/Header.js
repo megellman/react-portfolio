cd98fbbb4bd3a04ca4a078b85e4278b3c6bcab37
Megan Ellman <meganlellman@gmail.com>
2025-10-23 10:35:33 -0700

    nav name header navigates to root

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navigation from './Navigation';
+import { Link } from "react-router-dom";
 
 function Header() {
     return (
-        <div className='fixed-top bg-white d-flex align-items-center justify-content-between' style={{boxShadow:"0 4px 15px rgba(0, 0, 0, .1)"}}>
+        <div className='fixed-top bg-white d-flex align-items-center justify-content-between' style={{ boxShadow: "0 4px 15px rgba(0, 0, 0, .1)" }}>
             <Navigation />
-            
-            <h1 className='my-0 text-center p-2'>Megan Ellman</h1>
+            <Link to="/" className='text-dark text-decoration-none'>
+                <h1 className='my-0 text-center p-2'>Megan Ellman</h1>
+            </Link>
         </div>
     );
 }
