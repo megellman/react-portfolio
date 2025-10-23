2838458c8171370940a16090fd9a089a40703ffa
Megan Ellman <meganlellman@gmail.com>
2025-10-23 16:36:39 -0700

    updated styling

--- src/sections/About.js
+++ src/sections/About.js
@@ -1,12 +1,8 @@
-import React from 'react';
-// import headshot from '../assets/headshot.png'
 
 export default function AboutMe() {
     return (
         <div id='aboutMe' style={{ paddingBottom:"150px", paddingTop:"150px"}} className="d-flex flex-column justify-content-center align-items-center bg-primary text-light">
-            <div className='about-me'>
                 <p style={{ marginTop: '20px', fontSize: '1.2rem', width: '40rem', textAlign: 'center' }}>Since beginning my journey into software development, I’ve built responsive web applications, collaborated on team projects, and refined my skills through UC Berkeley’s fullstack bootcamp. With a background in SaaS operations and onboarding, I bring both technical and people-focused experience to my work. I’m naturally curious, detail-oriented, and always looking for ways to make code cleaner and user experiences smoother.</p>
-            </div>
         </div >
     )
 }
