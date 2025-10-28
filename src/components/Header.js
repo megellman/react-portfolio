22336a83f348b5523554cc4a56ec71398ecaa112
Megan Ellman <meganlellman@gmail.com>
2025-10-28 09:15:04 -0700

    updated font and sizing

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ function Header() {
     return (
         <div className='bg-light fixed-top d-flex align-items-center justify-content-between' style={{boxShadow:" rgba(17, 17, 26, 0.05) 0px 4px 16px, rgba(17, 17, 26, 0.05) 0px 8px 32px"}}>
             <Link to="/" className='text-dark text-decoration-none' >
-                <h1 className='my-0 text-center p-2'>Megan Ellman</h1>
+                <h1 className='display-1 my-0 text-center p-2' style={{fontSize: "5rem"}}>Megan Ellman</h1>
             </Link>
         </div>
     );
--- src/sections/Connect.js
+++ src/sections/Connect.js
@@ -4,8 +4,8 @@ export default function Connect() {
     return (
         <div className="d-flex justify-content-around align-items-center" style={{ height:"100vh" }}>
             <div>
-                <h1>Let’s Talk</h1>
-                <p>Whether it’s a project, opportunity, or idea — I’m just a message away</p>
+                <h1 style={{fontSize:"5rem"}}>Let’s Talk</h1>
+                <p className="lead">Whether it’s a project, opportunity, or idea — I’m just a message away</p>
             </div>
             <Link to="/Contact" className="btn btn-outline-dark btn-round btn-lg">
                Say Hi
