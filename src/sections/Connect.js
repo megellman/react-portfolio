65d7159a732db59ca9fd0bb2c6238db2be6494ff
Megan Ellman <meganlellman@gmail.com>
2026-01-10 14:57:29 -0800

    updated lets talk section

--- src/sections/Connect.js
+++ src/sections/Connect.js
@@ -5,7 +5,7 @@ export default function Connect() {
         <div className="d-flex justify-content-around align-items-center" style={{ height:"100vh" }}>
             <div>
                 <h1 style={{fontSize:"5rem"}}>Let’s Talk</h1>
-                <p className="lead">Whether it’s a project, opportunity, or idea — I’m just a message away</p>
+                <p className="lead">Whether it’s a technical support role, production-focused opportunity, or collaborative problem-solving challenge, I’m always open to a conversation.</p>
             </div>
             <Link to="/Contact" className="btn btn-outline-dark btn-round btn-lg">
                Say Hi
