728516c82386f9626e2edd3c1ede19856c6d0cda
Megan Ellman <meganlellman@gmail.com>
2026-01-10 14:49:25 -0800

    updated skills section

--- src/sections/Skills.js
+++ src/sections/Skills.js
@@ -1,38 +1,110 @@
-const frontEnd = ['HTML', 'CSS', 'JavaScript', 'jQuery', 'Responsive Design', 'React', 'Bootstrap'];
-const backEnd = ['APIs', 'Node', 'Express', 'MySQL', 'Sequelize', 'MongoDB', 'Mongoose', 'REST', 'GraphQL'];
-const toolsAndTechnologies = ['Git', 'GitHub', 'AJAX', 'Insomnia / Postman', 'Day.js', 'Unit Testing with Jest', 'Problem Solving', 'Pseudocode', 'REST API Design'];
+const webAndAppFundamentals = [
+  'HTML',
+  'CSS',
+  'JavaScript (ES6+)',
+  'React',
+  'Responsive UI Design',
+  'Client-side Error Handling'
+];
+
+const apisAndBackend = [
+  'REST APIs',
+  'GraphQL',
+  'Node.js',
+  'Express',
+  'Async Workflows',
+  'Data Validation',
+  'Error Handling'
+];
+
+const databases = [
+  'MySQL',
+  'Sequelize',
+  'MongoDB',
+  'Mongoose',
+  'Relational Data Concepts',
+  'Document-based Data Concepts'
+];
+
+const debuggingAndTooling = [
+  'Git',
+  'GitHub',
+  'Postman / Insomnia',
+  'Browser Developer Tools',
+  'Unit Testing with Jest'
+];
+
+const problemSolving = [
+  'Issue Reproduction',
+  'Root Cause Analysis',
+  'Pseudocode',
+  'Logical Decomposition',
+  'API Behavior Analysis'
+];
 
 function Skills() {
-    return (
-            <div className='container border rounded border-white bg-white' style={{ marginTop: "-90px", scrollMarginTop: "150px", zIndex: "2", position: "relative", padding: "20px", boxShadow: "rgba(0,0,0,0.15) 0 5px 15px" }}>
-                <div className='row justify-content-center'>
-                    <div className='col-4 text-center'>
-                        <h3>Front-end</h3>
-                        <ul className='list-inline'>
-                            {frontEnd.map((item, i) => (
-                                <li key={i}>{item}</li>
-                            ))}
-                        </ul>
-                    </div>
-                    <div className='col-4 text-center'>
-                        <h3>Back-end</h3>
-                        <ul className='list-inline'>
-                            {backEnd.map((item, i) => (
-                                <li key={i}>{item}</li>
-                            ))}
-                        </ul>
-                    </div>
-                    <div className='col-4 text-center'>
-                        <h3>Tools and Technologies</h3>
-                        <ul className='list-inline'>
-                            {toolsAndTechnologies.map((item, i) => (
-                                <li key={i}>{item}</li>
-                            ))}
-                        </ul>
-                    </div>
-                </div>
-            </div>
-    )
+  return (
+    <div
+      className="container border rounded border-white bg-white"
+      style={{
+        marginTop: '-90px',
+        scrollMarginTop: '150px',
+        zIndex: '2',
+        position: 'relative',
+        padding: '20px',
+        boxShadow: 'rgba(0,0,0,0.15) 0 5px 15px',
+      }}
+    >
+      <div className="row justify-content-center">
+        <div className="col-4 text-center">
+          <h3>Web & Applications</h3>
+          <ul className="list-unstyled">
+            {webAndAppFundamentals.map((item, i) => (
+              <li key={i}>{item}</li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="col-4 text-center">
+          <h3>APIs & Backend</h3>
+          <ul className="list-unstyled">
+            {apisAndBackend.map((item, i) => (
+              <li key={i}>{item}</li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="col-4 text-center">
+          <h3>Databases</h3>
+          <ul className="list-unstyled">
+            {databases.map((item, i) => (
+              <li key={i}>{item}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+
+      <div className="row justify-content-center mt-4">
+        <div className="col-6 text-center">
+          <h3>Debugging & Tooling</h3>
+          <ul className="list-unstyled">
+            {debuggingAndTooling.map((item, i) => (
+              <li key={i}>{item}</li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="col-6 text-center">
+          <h3>Problem Solving</h3>
+          <ul className="list-unstyled">
+            {problemSolving.map((item, i) => (
+              <li key={i}>{item}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Skills;
