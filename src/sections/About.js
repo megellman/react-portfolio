79e6d6af6320c9d8412651db8ab9280ff5e7ecc8
Megan Ellman <meganlellman@gmail.com>
2026-01-12 09:40:05 -0800

    updated about section

--- src/sections/About.js
+++ src/sections/About.js
@@ -2,7 +2,7 @@
 export default function AboutMe() {
     return (
         <div id='aboutMe' style={{ paddingBottom:"150px", paddingTop:"150px"}} className="d-flex flex-column justify-content-center align-items-center bg-primary text-light">
-                <p style={{ marginTop: '20px', width: '100rem', fontSize: '1.25rem' }}>I’m a Technical Support Specialist with a background in high-volume SaaS support, onboarding operations, and full-stack web development. I specialize in troubleshooting complex issues, validating configurations, reproducing bugs, and translating technical problems into clear, actionable solutions. <br /> <br /> Through UC Berkeley’s full-stack bootcamp and hands-on SaaS support experience, I’ve developed a strong foundation in APIs, system behavior, debugging, and documentation. I’m detail-oriented, calm under pressure, and motivated by roles where reliability, communication, and operational excellence directly impact users.
+                <p style={{ marginTop: '20px', width: '100rem', fontSize: '1.5rem' }}>I’m a Technical Support Specialist with a background in high-volume SaaS support, onboarding operations, and full-stack web development. I specialize in troubleshooting complex issues, validating configurations, reproducing bugs, and translating technical problems into clear, actionable solutions. <br /> <br /> Through UC Berkeley’s full-stack bootcamp and hands-on SaaS support experience, I’ve developed a strong foundation in APIs, system behavior, debugging, and documentation. I’m detail-oriented, calm under pressure, and motivated by roles where reliability, communication, and operational excellence directly impact users.
                 </p>
         </div >
     )
