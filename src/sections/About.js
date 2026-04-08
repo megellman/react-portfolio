ac32302b22089e0374c77ebd55bbfacb30fc95dd
Megan Ellman <meganlellman@gmail.com>
2026-04-08 15:16:23 -0700

    updated about section

--- src/sections/About.js
+++ src/sections/About.js
@@ -1,11 +1,9 @@
 
 export default function AboutMe() {
     return (
-        <div id='aboutMe' style={{ paddingBottom:"150px", paddingTop:"150px"}} className="d-flex flex-column justify-content-center align-items-center bg-primary text-light">
-                <p style={{ marginTop: '20px', width: '100rem', fontSize: '1.5rem' }}>I’m a Technical Support Specialist with experience supporting high-volume SaaS platforms and collaborating closely with Product and Engineering teams. I specialize in troubleshooting complex issues, reproducing bugs, and translating technical problems into clear, durable solutions.
-
-Through hands-on support work and UC Berkeley’s full-stack program, I’ve built a strong foundation in APIs, debugging, system behavior, and documentation. I thrive in roles where reliability, clarity, and deep product understanding directly impact users, with a long-term interest in growing into engineering-adjacent work.
-                </p>
+        <div id='aboutMe' style={{ paddingBottom: "150px", paddingTop: "150px" }} className="d-flex flex-column justify-content-center align-items-center bg-primary text-light">
+            <p style={{ marginTop: '20px', fontSize: '1.5rem', padding: "0 200px"}}>Technical support specialist with 2 years of SaaS experience in API integration support, systems administration, and customer onboarding. I build processes that reduce errors, create documentation that scales, and translate complex technical problems into clear solutions for non-technical stakeholders. I thrive at the intersection of technical depth and customer or operational impact.
+            </p>
         </div >
     )
 }
